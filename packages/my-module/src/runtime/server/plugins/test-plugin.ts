08e48e77654abfa03cad9ed7b4ff32eeb2762d59
Frederik Prijck <frederik.prijck@okta.com>
2025-06-30 13:32:51 +0200

    Update test-plugin.ts

--- packages/my-module/src/runtime/server/plugins/test-plugin.ts
+++ packages/my-module/src/runtime/server/plugins/test-plugin.ts
@@ -2,7 +2,7 @@ import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin';
 
 export default defineNitroPlugin(async (nitroApp) => {
   // Importing the alias path
-  const factoryModule = await import('#my-session-store';);
+  const factoryModule = await import('#my-session-store');
   const sessionStoreInstance = factoryModule.default;
 
   // Logging the instance created by the factory
